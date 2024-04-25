b8b406cb2dcb342710671f4f3e5ee2db7c62ab38
Peter Lithammer <peter.lithammer@gmail.com>
2024-04-25 20:15:01 +0200

    Add ruff language server to python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -12,6 +12,9 @@ lsp.start({
     'requirements.txt',
   }),
   settings = {
+    pyright = {
+      disableOrganizeImports = true,
+    },
     python = {
       analysis = {
         autoSearchPaths = true,
@@ -21,3 +24,22 @@ lsp.start({
     },
   },
 })
+
+if vim.fn.executable('ruff') == 1 then
+  lsp.start({
+    name = 'ruff',
+    cmd = { 'ruff', 'server', '--preview' },
+    workspace_folders = ws.find({ 'pyproject.toml', 'ruff.toml', '.ruff.toml' }),
+    -- https://github.com/astral-sh/ruff/blob/main/crates/ruff_server/docs/setup/NEOVIM.md#tips
+    on_attach = function(client)
+      if client.name == 'ruff' then
+        client.server_capabilities.hoverProvider = false
+      end
+    end,
+    init_options = {
+      settings = {
+        args = {},
+      },
+    },
+  })
+end
