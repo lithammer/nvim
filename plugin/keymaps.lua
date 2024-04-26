6f10acff6918b169b6343640bcc36b21ff87097c
Peter Lithammer <peter.lithammer@gmail.com>
2024-04-26 23:01:47 +0200

    Remove custom mappings now provided by default in 0.10

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,8 +1,14 @@
 local keymap = vim.keymap
 
+local Methods = vim.lsp.protocol.Methods
+
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
 
     local function keymap_set(mode, lhs, rhs, opts)
       opts = vim.tbl_extend('force', opts or {}, { buffer = bufnr })
@@ -19,39 +25,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
     keymap_set('n', 'gP', '<cmd>Glance definitions<cr>')
     keymap_set('n', 'gD', vim.lsp.buf.declaration)
     keymap_set('n', 'gy', '<cmd>Glance type_definitions<cr>')
-    keymap_set('n', 'gI', '<cmd>Glance implementations<cr>')
+
+    if client.supports_method(Methods.textDocument_implementation) then
+      keymap_set('n', 'gI', '<cmd>Glance implementations<cr>')
+      -- keymap_set('n', 'gI', vim.lsp.buf.implementation)
+      -- keymap_set('n', 'g<C-I>', vim.lsp.buf.implementation, { buffer = args.buf })
+    end
+
     -- keymap_set('n', 'gy', vim.lsp.buf.type_definition)
-    -- keymap_set('n', 'gI', vim.lsp.buf.implementation)
-    keymap_set('i', '<c-s>', vim.lsp.buf.signature_help)
-    keymap_set('n', 'crn', vim.lsp.buf.rename)
     -- keymap_set('n', 'gr', vim.lsp.buf.references)
     keymap_set('n', 'gr', '<cmd>Glance references<cr>')
     keymap_set('n', 'gO', vim.lsp.buf.document_symbol)
     keymap_set('n', 'gW', vim.lsp.buf.workspace_symbol)
-    keymap_set('n', 'cra', vim.lsp.buf.code_action)
-    keymap_set('n', 'crA', function()
-      code_action_kind('source')
-    end)
-    keymap_set('n', '<leader>r', function()
-      code_action_kind('refactor')
-    end)
-
-    keymap_set('n', '[d', function()
-      vim.diagnostic.goto_prev({ wrap = false, float = false })
-    end)
-    keymap_set('n', ']d', function()
-      vim.diagnostic.goto_next({ wrap = false, float = false })
-    end)
-
-    keymap_set('n', '<c-w>d', function()
-      vim.diagnostic.open_float({ border = 'rounded' })
-    end, {
-      desc = 'Open a floating window showing diagnostics under the cursor',
-    })
-    keymap_set('n', '<c-w><c-d>', function()
-      vim.diagnostic.open_float({ border = 'rounded' })
-    end, {
-      desc = 'Open a floating window showing diagnostics under the cursor',
-    })
+    keymap_set('n', 'crR', function() code_action_kind('source') end)
+    keymap_set('n', '<leader>r', function() code_action_kind('refactor') end)
   end,
 })
