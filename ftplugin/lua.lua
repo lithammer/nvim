ea95a45bfe99a4e740e4d309d36079aab58db32f
Peter Lithammer <peter.lithammer@gmail.com>
2024-06-02 22:16:20 +0200

    Replace neodev (deprecated) with lazydev

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,12 +1,15 @@
 local lsp = require('lsp')
 local ws = require('lsp.ws')
 
-require('neodev').setup({ lspconfig = false })
+local now = MiniDeps.now
+
+now(function()
+  require('lazydev').setup()
+end)
 
 lsp.start({
   name = 'lua-language-server',
   cmd = { 'lua-language-server' },
-  before_init = require('neodev.lsp').before_init,
   workspace_folders = ws.find({ '.luarc.json', '.luarc.jsonc' }),
   settings = {
     Lua = {
