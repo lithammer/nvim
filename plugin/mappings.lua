924699e7d87b6ca50f22b6a8839644fd55938901
Peter Lithammer <peter.lithammer@gmail.com>
2024-07-16 23:04:38 +0200

    Remove keymap indirection

--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -2,25 +2,6 @@ local keymap = vim.keymap
 
 local Methods = vim.lsp.protocol.Methods
 
--- Tentative Neovim 0.11 LSP mappings.
-do
-  vim.keymap.set('n', 'grn', function()
-    vim.lsp.buf.rename()
-  end, { desc = 'LSP: Rename' })
-
-  vim.keymap.set({ 'n', 'x' }, 'gra', function()
-    vim.lsp.buf.code_action()
-  end, { desc = 'LSP: Code action' })
-
-  vim.keymap.set('n', 'grr', function()
-    vim.lsp.buf.references()
-  end, { desc = 'LSP: References' })
-
-  vim.keymap.set('i', '<C-S>', function()
-    vim.lsp.buf.signature_help()
-  end, { desc = 'LSP: Signature help' })
-end
-
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local bufnr = args.buf
@@ -29,38 +10,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
       return
     end
 
-    local function keymap_set(mode, lhs, rhs, opts)
-      opts = vim.tbl_extend('force', opts or {}, { buffer = bufnr })
-      keymap.set(mode, lhs, rhs, opts)
-    end
-
     ---@param kind string
     local function code_action_kind(kind)
       ---@diagnostic disable-next-line: missing-fields
       vim.lsp.buf.code_action({ context = { only = { kind } } })
     end
 
-    keymap_set('n', 'gd', vim.lsp.buf.definition)
-    keymap_set('n', 'gP', '<cmd>Glance definitions<cr>')
-    keymap_set('n', 'gD', vim.lsp.buf.declaration)
-    keymap_set('n', 'gy', '<cmd>Glance type_definitions<cr>')
+    keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+    keymap.set('n', 'gP', '<cmd>Glance definitions<cr>', { buffer = bufnr })
+    keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+    keymap.set('n', 'gy', '<cmd>Glance type_definitions<cr>', { buffer = bufnr })
 
     if client.supports_method(Methods.textDocument_implementation) then
-      keymap_set('n', 'gI', '<cmd>Glance implementations<cr>')
-      -- keymap_set('n', 'gI', vim.lsp.buf.implementation)
-      -- keymap_set('n', 'g<C-I>', vim.lsp.buf.implementation, { buffer = args.buf })
+      keymap.set('n', 'gI', '<cmd>Glance implementations<cr>', { buffer = bufnr })
+      -- keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr })
+      -- keymap.set('n', 'g<C-I>', vim.lsp.buf.implementation, { buffer = bufnr })
     end
 
-    -- keymap_set('n', 'gy', vim.lsp.buf.type_definition)
-    -- keymap_set('n', 'gr', vim.lsp.buf.references)
-    keymap_set('n', 'gr', '<cmd>Glance references<cr>')
-    keymap_set('n', 'gO', vim.lsp.buf.document_symbol)
-    keymap_set('n', 'gW', vim.lsp.buf.workspace_symbol)
-    keymap_set('n', 'crR', function()
+    -- keymap.set('n', 'gy', vim.lsp.buf.type_definition, { buffer = bufnr })
+    keymap.set('n', 'grr', '<cmd>Glance references<cr>', { buffer = bufnr })
+    keymap.set('n', 'gO', vim.lsp.buf.document_symbol, { buffer = bufnr })
+    keymap.set('n', 'gW', vim.lsp.buf.workspace_symbol, { buffer = bufnr })
+    keymap.set('n', 'crR', function()
       code_action_kind('source')
-    end)
-    keymap_set('n', '<leader>r', function()
+    end, { buffer = bufnr })
+    keymap.set('n', '<leader>r', function()
       code_action_kind('refactor')
-    end)
+    end, { buffer = bufnr })
   end,
 })
