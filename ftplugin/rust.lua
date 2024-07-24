84b35abea2ea3308721add082e087f8be1cb0537
Peter Lithammer <peter.lithammer@gmail.com>
2024-07-24 23:38:18 +0200

    Correctly initialize rust-analyzer

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,21 +1,24 @@
 local lsp = require('lsp')
 local ws = require('lsp.ws')
 
+local settings = {
+  ['rust-analyzer'] = {
+    check = {
+      command = 'clippy',
+    },
+    cargo = {
+      features = 'all',
+    },
+    completion = {
+      postfix = { enable = false },
+    },
+  },
+}
+
 lsp.start({
   name = 'rust-analyzer',
   cmd = { 'rust-analyzer' },
   workspace_folders = ws.find('Cargo.toml'),
-  settings = {
-    ['rust-analyzer'] = {
-      check = {
-        command = 'clippy',
-      },
-      cargo = {
-        features = 'all',
-      },
-      completion = {
-        postfix = { enable = false },
-      },
-    },
-  },
+  settings = settings,
+  init_options = settings['rust-analyzer'],
 })
