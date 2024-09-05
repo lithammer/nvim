a9b06f5007e3d4f8eccd4fc486257147da2fcc15
Peter Lithammer <peter.lithammer@gmail.com>
2024-09-05 22:52:37 +0200

    Add `fish_indent` formatter

--- plugin/format.lua
+++ plugin/format.lua
@@ -16,6 +16,7 @@ later(function()
       },
     },
     formatters_by_ft = {
+      fish = { 'fish_indent' },
       javascript = { 'biome-check', 'prettier', stop_after_first = true },
       lua = { 'stylua' },
       nim = { 'nph', 'nimpretty', stop_after_first = true },
