ef8f71f604637c7ebef7538167119797e1ae794d
Peter Lithammer <peter.lithammer@embark-studios.com>
2024-09-09 15:41:58 +0200

    Improve handling of third-party modules in Go
    This might not be ideal since it will spin up a new server when for
    example jumping to symbol in a third-party library. But at least it will
    re-use the workspace for all third-party libraries.

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,10 +1,28 @@
 local lsp = require('lsp')
 local ws = require('lsp.ws')
 
+local function workspace_folders()
+  local name = vim.api.nvim_buf_get_name(0)
+
+  -- Check if the current buffer is a third-party module.
+  local obj = vim.system({ 'go', 'env', 'GOMODCACHE' }, { text = true }):wait()
+  if obj.code == 0 then
+    local modcache = vim.trim(obj.stdout or '')
+    local is_module = name:sub(1, #modcache) == modcache
+    if is_module then
+      return ws.fname_to_workspace_folder(modcache)
+    end
+  else
+    vim.notify('`go env GOMODCACHE` command failed: ' .. (obj.stderr or ''), vim.log.levels.ERROR)
+  end
+
+  return ws.find('go.work') or ws.find('go.mod')
+end
+
 lsp.start({
   name = 'gopls',
   cmd = { 'gopls', 'serve' },
-  workspace_folders = ws.find('go.work') or ws.find('go.mod'),
+  workspace_folders = workspace_folders(),
   settings = {
     gopls = {
       analyses = {
