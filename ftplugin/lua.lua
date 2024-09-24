473e10207a2819e26227d905dfb4e648b537917c
Peter Lithammer <peter.lithammer@gmail.com>
2024-09-24 22:28:58 +0200

    Add Git and cwd root markers to LuaLS

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -10,6 +10,6 @@ end)
 lsp.start({
   name = 'lua_ls',
   cmd = { 'lua-language-server' },
-  workspace_folders = ws.find({ '.luarc.json', '.luarc.jsonc' }),
+  workspace_folders = ws.find({ '.luarc.json', '.luarc.jsonc' }) or ws.git() or ws.cwd(),
   settings = {},
 })
