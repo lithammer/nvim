ebb6e0669bfceaa89cacb7f59d4501817b5789de
Peter Lithammer <peter.lithammer@gmail.com>
2024-10-09 23:09:18 +0200

    Improve LuaLS workspace root for single files
    Prefer the buffer's directory instead of `$HOME` because LuaLS throws
    an error if you try to use it as workspace root.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,6 +2,7 @@ local lsp = require('lsp')
 local ws = require('lsp.ws')
 
 local now = MiniDeps.now
+local fn = vim.fn
 
 now(function()
   require('lazydev').setup({
@@ -16,9 +17,18 @@ now(function()
   })
 end)
 
+local workspace_folders = ws.find({ '.luarc.json', '.luarc.jsonc' }) or ws.git()
+if not workspace_folders then
+  if fn.getcwd() ~= vim.env.HOME then
+    workspace_folders = ws.cwd()
+  else
+    workspace_folders = ws.bufdir()
+  end
+end
+
 lsp.start({
   name = 'lua_ls',
   cmd = { 'lua-language-server' },
-  workspace_folders = ws.find({ '.luarc.json', '.luarc.jsonc' }) or ws.git() or ws.cwd(),
+  workspace_folders = workspace_folders,
   settings = {},
 })
