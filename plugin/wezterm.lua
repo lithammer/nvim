da002528625baebbe6c7196399474d5ff2703ad0
Peter Lithammer <peter.lithammer@embark-studios.com>
2024-10-29 22:47:52 +0100

    Fix OSC 52 paste issues on Wezterm
    Wezterm apparently doesn't allow pasting from the clipboard. So let's
    diable it and reply on ctrl-shift-v/cmd-v.

---
+++ plugin/wezterm.lua
@@ -0,0 +1,23 @@
+if vim.env.TERM:find('wezterm') and vim.env.SSH_TTY then
+  vim.o.clipboard = 'unnamedplus'
+
+  local fn = vim.fn
+  local function paste()
+    return {
+      fn.split(fn.getreg(''), '\n'),
+      fn.getregtype(''),
+    }
+  end
+
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+  }
+end
