0f64585b34c292217cfecbb8d24841961c22b787
Peter Lithammer <peter.lithammer@embark-studios.com>
2024-11-04 08:30:52 +0100

    Change proto LSP server to github.com/coder3101/protols

--- ftplugin/proto.lua
+++ ftplugin/proto.lua
@@ -1,12 +1,6 @@
 local lsp = require('lsp')
 
-local fn, fs = vim.fn, vim.fs
-
 lsp.start({
-  name = 'protobuf-language-server',
-  cmd = {
-    'protobuf-language-server',
-    '-logs',
-    fs.joinpath(fn.stdpath('log') --[[@as string]], 'protobuf-language-server.log'),
-  },
+  name = 'protols',
+  cmd = { 'protols' },
 })
