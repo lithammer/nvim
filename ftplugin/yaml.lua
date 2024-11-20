0350b5de18609c6c2cfeb1c587436343f2810274
Peter Lithammer <peter.lithammer@embark-studios.com>
2024-11-20 09:03:05 +0100

    Add workspace folders to yaml language server

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,7 +1,9 @@
 local lsp = require('lsp')
+local ws = require('lsp.ws')
 
 lsp.start({
   name = 'yaml-language-server',
   cmd = { 'yaml-language-server', '--stdio' },
+  workspace_folders = ws.find({ '.yamllint', '.yamllint.yaml', '.yamllint.yml' }),
   settings = {},
 })
