b20431ed6051126b08e15aba271fd6a66b613ebe
Peter Lithammer <peter.lithammer@embark-studios.com>
2024-12-11 15:16:47 +0100

    Redraw all statuslines for gutentags events
    It was lingering in unfocused statuslines otherwise.

--- plugin/statusline.lua
+++ plugin/statusline.lua
@@ -52,7 +52,7 @@ vim.api.nvim_create_autocmd('DiagnosticChanged', {
 
 vim.api.nvim_create_autocmd('User', {
   pattern = { 'GutentagsUpdating', 'GutentagsUpdated' },
-  command = 'redrawstatus',
+  command = 'redrawstatus!',
 })
 
 ---@param focused boolean
