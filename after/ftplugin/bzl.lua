cbe56d11ed0ad28719c287c8a027f1073d40fc10
Peter Lithammer <peter.lithammer@embark-studios.com>
2025-01-16 22:49:10 +0100

    Add starpls to bzl files

---
+++ after/ftplugin/bzl.lua
@@ -0,0 +1,24 @@
+local lsp = require('lsp')
+
+local root_dir = vim.fs.root(0, 'WORKSPACE')
+
+-- lsp.start({
+--   name = 'bazel-lsp',
+--   cmd = { 'bazel-lsp' },
+--   root_dir = root_dir,
+--   settings = {},
+-- })
+
+lsp.start({
+  name = 'starpls',
+  cmd = { 'starpls' },
+  root_dir = root_dir,
+  settings = {},
+})
+
+-- lsp.start({
+--   name = 'starlark',
+--   cmd = { 'starlark', '--lsp' },
+--   root_dir = root_dir,
+--   settings = {},
+-- })
