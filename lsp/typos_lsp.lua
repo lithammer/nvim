85f398f02aa389cacc79a9c66716f66ff7913d01
Peter Lithammer <peter.lithammer@embark-studios.com>
2025-04-14 11:08:10 +0200

    Add pyroject.toml and Cargo.toml as typos-lsp root markers

--- lsp/typos_lsp.lua
+++ lsp/typos_lsp.lua
@@ -2,7 +2,7 @@
 return {
   cmd = { 'typos-lsp' },
   cmd_env = { RUST_LOG = 'error' },
-  root_markers = { 'typos.toml', '_typos.toml', '.typos.toml' },
+  root_markers = { 'typos.toml', '_typos.toml', '.typos.toml', 'pyroject.toml', 'Cargo.toml' },
   filetypes = {
     'go',
     'lua',
