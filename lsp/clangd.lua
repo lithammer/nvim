3ef6ea35970d319d6da879d3397d16a6d1485342
Peter Lithammer <peter.lithammer@gmail.com>
2025-06-04 14:13:19 +0200

    Port clangd offsetEncoding capability from nvim-lspconfig
    - https://github.com/neovim/nvim-lspconfig/pull/3887

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,6 +1,19 @@
+---@class ClangdInitializeResult: lsp.InitializeResult
+---@field offsetEncoding? string
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'clangd', '--clang-tidy' },
   filetypes = { 'c', 'cpp' },
   root_markers = { '.clangd', 'compile_commands.json' },
+  capabilities = {
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+  ---@param init_result ClangdInitializeResult
+  on_init = function(client, init_result)
+    ---@diagnostic disable-next-line: undefined-field
+    if init_result.offsetEncoding then
+      client.offset_encoding = init_result.offsetEncoding
+    end
+  end,
 }
