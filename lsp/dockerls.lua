f580a98645f711b0bbbf0079060ea3a8f852793d
Peter Lithammer <peter.lithammer@gmail.com>
2025-08-01 23:11:24 +0200

    Use official Dockerfile language server
    https://github.com/docker/docker-language-server

--- lsp/dockerls.lua
+++ lsp/dockerls.lua
@@ -1,5 +1,10 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'docker-langserver', '--stdio' },
-  filetypes = { 'dockerfile' },
+  cmd = { 'docker-language-server', 'start', '--stdio' },
+  filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  root_markers = {
+    'Dockerfile',
+    'docker-compose.yaml',
+    'docker-compose.yml',
+  },
 }
