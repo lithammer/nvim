8434ffad6398cb93bf6a1d3e0acdffa07bc2eb5f
Peter Lithammer <peter.lithammer@gmail.com>
2025-09-05 19:46:33 +0200

    Configure autocmd before vim.pack.add
    The autocmd will run too late otherwise in initial installation.
    Consider using stable instead?

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -1,34 +1,3 @@
-local build_blink = function(params)
-  vim.notify('Building blink.cmp', vim.log.levels.INFO)
-  local cmd = { 'cargo', 'build', '--release' }
-  local env = { CARGO_NET_GIT_FETCH_WITH_CLI = 'true' }
-  local opts = { cwd = params.path, env = env }
-
-  vim.system(cmd, opts, function(obj)
-    if obj.code == 0 then
-      vim.notify('Building blink.cmp done', vim.log.levels.INFO)
-    else
-      vim.notify('Building blink.cmp failed', vim.log.levels.ERROR)
-    end
-  end)
-end
-
-do
-  local group = vim.api.nvim_create_augroup('blink_cmp_update', { clear = true })
-  vim.api.nvim_create_autocmd('PackChanged', {
-    desc = 'Build blink.cmp',
-    group = group,
-    callback = function(event)
-      local spec = event.data.spec
-      local kind = event.data.kind
-
-      if spec and spec.name == 'blink.cmp' and (kind == 'install' or kind == 'update') then
-        build_blink(params)
-      end
-    end,
-  })
-end
-
 require('blink-cmp').setup({
   keymap = {
     preset = 'default',
