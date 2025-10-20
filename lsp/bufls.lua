2c553d4334e8957d92ec569ea28d6705b1f69d38
Peter Lithammer <peter.lithammer@gmail.com>
2025-10-20 21:25:12 +0200

    `buf beta lsp` is deprecated as of v1.59.0
    https://github.com/bufbuild/buf/releases/tag/v1.59.0

--- lsp/bufls.lua
+++ lsp/bufls.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text' },
+  cmd = { 'buf', 'lsp', 'serve', '--timeout=0', '--log-format=text' },
   filetypes = { 'proto' },
   root_markers = { 'buf.yaml' },
   workspace_required = true,
