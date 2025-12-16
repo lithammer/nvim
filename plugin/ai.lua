4e1252e785dccdd7f76863b3e0fb245062d1920d
Peter Lithammer <peter.lithammer@gmail.com>
2025-12-16 23:25:53 +0100

    Update OpenCode mappings to use new operator mapping

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -11,19 +11,23 @@ do
   vim.keymap.set({ 'n', 'x' }, '<leader>ox', function()
     require('opencode').select()
   end, { desc = 'Execute OpenCode action' })
-  vim.keymap.set({ 'n', 'x' }, 'ga', function()
-    require('opencode').prompt('@this')
-  end, { desc = 'Add to OpenCode' })
   vim.keymap.set({ 'n', 't' }, '<C-.>', function()
     require('opencode').toggle()
   end, { desc = 'Toggle OpenCode' })
+
+  vim.keymap.set({ 'n', 'x' }, 'go', function()
+    return require('opencode').operator('@this ')
+  end, { expr = true, desc = 'Add range to OpenCode' })
+  vim.keymap.set('n', 'goo', function()
+    return require('opencode').operator('@this ') .. '_'
+  end, { expr = true, desc = 'Add line to OpenCode' })
+
   vim.keymap.set('n', '<S-C-u>', function()
     require('opencode').command('session.half.page.up')
-  end, { desc = 'opencode half page up' })
-
+  end, { desc = 'OpenCode half page up' })
   vim.keymap.set('n', '<S-C-d>', function()
     require('opencode').command('session.half.page.down')
-  end, { desc = 'opencode half page down' })
+  end, { desc = 'OpenCode half page down' })
 end
 
 do
