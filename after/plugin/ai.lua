a7f6094d4fe8bb66f0f0c77c45b1e0e2b69bf690
Peter Lithammer <peter.lithammer@embark-studios.com>
2026-03-05 14:10:05 +0100

    Enable agentic and add keymaps

--- after/plugin/ai.lua
+++ after/plugin/ai.lua
@@ -2,6 +2,23 @@ vim.g.copilot_filetypes = {
   gitcommit = true,
 }
 
+do
+  require('agentic').setup({
+    provider = vim.env.AGENTIC_PROVIDER or 'codex-acp',
+  })
+
+  vim.keymap.set({ 'n', 'v', 'i' }, '<C-\\>', function()
+    require('agentic').toggle()
+  end, { desc = 'Toggle Agentic Chat' })
+
+  vim.keymap.set({ 'n', 'v' }, "<C-'>", function()
+    require('agentic').add_selection_or_file_to_context()
+  end, { desc = 'Add file or selection to Agentic to Context' })
+
+  vim.keymap.set({ 'n', 'v', 'i' }, '<C-,>', function()
+    require('agentic').new_session()
+  end, { desc = 'New Agentic Session' })
+end
 
 do
   vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
